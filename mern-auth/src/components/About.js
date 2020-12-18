71dcba9a341edad0dddf054ebc48c143deb94acf
swolepenguin <ashtonm223@gmail.com>
2020-12-18 12:17:09 -0500

    component additions

---
+++ mern-auth/src/components/About.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <p>latin latin</p>
+        </div>
+    )
+}
+export default About
---
+++ mern-auth/src/components/Footer.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+const Footer = () => {
+    return(
+     <footer className= "footers bg_dark"> 
+     <div className= 'container text-center'>
+         <span className= "text-muted" >82020 built by rome bell</span>
+     </div>
+     </footer>
+    )
+}
+export default Footer
